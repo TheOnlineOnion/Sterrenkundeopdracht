5a07b17cb128b46c9f9a065b60a7e1bd8c6a6986
Merijn Post <merijnpost2006@gmail.com>
2026-02-05 18:36:28 +0100

    correcte countourfill en dubbelplot

--- merijns_test_bed.py
+++ merijns_test_bed.py
@@ -1,4 +1,3 @@
-#%% Afbeelding
 import numpy as np
 import astropy as ap
 from astropy.io import fits
@@ -7,7 +6,7 @@ from matplotlib.colors import LogNorm
 from astropy.utils.data import download_file
 
 # Je moet hieronder zelf handmatig de file location veranderen naar waar jij je fits files hebt opgeslagen
-path = "C:/Users/MeintPostSecuMailerC/Documents/vnv_natuurkunde/Natuurkunde Opdracht/FITS images/Dubbelcluster_2.fit"
+path = "C:/Users/MeintPostSecuMailerC/Documents/vnv_natuurkunde/Natuurkunde Opdracht/FITS images/dubbelcluster_overbelicht.fit"
 
 informatie = fits.getheader(path)
 print(informatie)
@@ -16,9 +15,8 @@ hdu_list = fits.open(path)
 image_data_l = hdu_list[0].data
 hdu_list.close()
 
-# Afbeelding croppen, het is 0<=y<3330 en 0<=x<05363
-image_data = image_data_l[0:3330, 0:5363]
-image_data = image_data*0.75
+# Afbeelding croppen, het is 0<=y<3330 en 0<=x<5363
+image_data = image_data_l#[0:3330, 0:5363]
 
 # l is hoe vaak je de afbeelding wil stacken
 l = 1
@@ -30,11 +28,19 @@ print(f"This is stacked {l} times")
 #Histogram om vmin en vmax te verkrijgen
 #image_hist = plt.hist(final_image.flatten(), bins="auto")
 
-fig = plt.figure()
-ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+fig, axes = plt.subplots(2, 1)
+fig.set_figheight(6)
+fig.set_figwidth(6)
+
 #vmin en vmax verkegen uit de histogram
-ax.imshow(final_image, cmap="grey", vmin=1.5e3, vmax=2.1e3) 
-#ax.contour(final_image, levels=10, colors='red', linewidths=1.5)
+axes[0].imshow(final_image, cmap="grey", vmin=2e3, vmax=2.5e3, origin='lower', aspect='auto') 
+
+
+#Code voor countourkaart
+cs = plt.contourf(final_image, levels=[2500, 2501], colors='red', extend='both', aspect='equal')
+cs.cmap.set_over('red')
+cs.cmap.set_under('blue')
+cs.changed()
+axes[1] = cs
 
 plt.show()
-
