4f2582c5dd586e744f58082c5450c2d9ad2c6f80
Merijn Post <merijnpost2006@gmail.com>
2026-02-18 15:49:19 +0100

    DAOStarFinder werkende

--- merijns_starfinder_test.py
+++ merijns_starfinder_test.py
@@ -23,32 +23,53 @@ ccd_image = CCDData.read(path, unit="adu")
 image_data = ccd_image.data
 header = ccd_image.header
 
-norm_image = ImageNormalize(ccd_image, interval=AsymmetricPercentileInterval(30, 99.5))
-
-# 2-D background estimation
+# We gebruiken de sigma-clip functionaliteit om een 2 dimensionale waarde plot te maken van de achtergrond, zodat we deze er af kunnen halen en overal een locale gemiddelde achtergrond waarde van 0 kunnen bereiken
 sigma_clip = SigmaClip(sigma=3., maxiters=5)
 bkg_estimator = MeanBackground()
 bkg = Background2D(ccd_image, box_size=200, filter_size=(11, 11), 
                    mask=ccd_image.mask, sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
 
-# Calculate the 2D background subtraction, maintaining metadata, unit, and mask
-ccd_2d_bkgdsub = ccd_image.subtract(bkg.background)
+# We verwijderen nu de locale gemmidelde achtergrond waarde van elke pixel die niet een ster is, zodat de steren better gedetecteerd kunnen worden
+ccd_2d_bkgdsubtr = ccd_image.subtract(bkg.background)
 
-# Set up the figure with subplots
+mean, median, std = sigma_clipped_stats(ccd_2d_bkgdsubtr.data, sigma=3, maxiters=5)
+
+# We maaken een mask array aan
+mask = np.zeros_like(ccd_2d_bkgdsubtr, dtype=bool)
+
+# en we maskeren de buitenste 10 pixels, aangezien die geen echte data representeren op de plot
+mask[:10, :] = mask[-10:, :] = mask[:, :10] = mask[:, -10:] = 1.0
+
+# We maken een object aan van de DAOStarFinder classe, met een full width half maximum van 6.2
+fwhm = 6.2
+daofinder = DAOStarFinder(threshold=6 * std, fwhm=fwhm)
+
+# Zoek nu de sterren, zonder de buitenlaag mee te nemen
+sources = daofinder(ccd_2d_bkgdsubtr.data, mask=mask)
+print(f"Found {len(sources)} sources in the image")
+
+# We plaatsen cirkels rondom de x en y waardes van de sterren, om te zien welke sterren de starfinder wel en niet pakt
+positions = np.transpose((sources['xcentroid'], sources['ycentroid']))
+apertures = CircularAperture(positions, r=3 * fwhm)
+
+# De afbeelding plotten, met colorbar
 fig, ax1 = plt.subplots(1, 1, figsize=(8, 8), sharey=True)
 plt.tight_layout()
 
-
+# Labels voor de assen aanmaken
 ax1.set_ylabel('Y (pixels)')
 ax1.set_xlabel('X (pixels)')
 
-# Plot the 2D-subtracted data
-norm_image_sub = ImageNormalize(ccd_2d_bkgdsub, interval=AsymmetricPercentileInterval(30, 99.5))
+# De data zonder achtergrond nomralizeren en daarna plotten
+norm_image_sub = ImageNormalize(ccd_2d_bkgdsubtr, interval=AsymmetricPercentileInterval(30, 99.5))
 
-fitsplot = ax1.imshow(np.ma.masked_where(ccd_2d_bkgdsub.mask, ccd_2d_bkgdsub), norm=norm_image_sub, cmap='viridis')
+fitsplot = ax1.imshow(np.ma.masked_where(ccd_2d_bkgdsubtr.mask, ccd_2d_bkgdsubtr), norm=norm_image_sub, cmap='viridis')
 ax1.set_title('2D Background-Subtracted Data')
 
-# Define the colorbar...
+# De cirkels plotten
+apertures.plot(color="red")
+
+# De colorbar aanmaken en op de plot plaatsen
 cbar = fig.colorbar(fitsplot, location='right', shrink=0.6)
 
 cbar.set_label(r'Counts ({})'.format(ccd_image.unit.to_string('latex')),
