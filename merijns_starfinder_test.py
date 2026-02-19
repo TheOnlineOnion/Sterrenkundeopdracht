48fdcb393615dbecffa8b7f2037225b17d2ea370
Merijn Post <merijnpost2006@gmail.com>
2026-02-19 23:29:10 +0100

    Ik haat subplots

--- merijns_starfinder_test.py
+++ merijns_starfinder_test.py
@@ -51,20 +51,27 @@ print(f"Found {len(sources)} sources in the image")
 positions = np.transpose((sources['xcentroid'], sources['ycentroid']))
 apertures = CircularAperture(positions, r=3 * fwhm)
 
-# De bevolkingsdichtheids diagram maken. We hebben een afbeelding van dy=3672 en dx = 5496. We maken blokjes van 51 hoog en 51 breed zodat we 72 blokjes verticaal en 107 blokjes horizontaal hebben
+
+# De bevolkingsdichtheids diagram maken. We hebben een afbeelding van dy=3672 en dx = 5496. We maken blokjes van 102 hoog en 102 breed zodat we 72 blokjes verticaal en 107 blokjes horizontaal hebben
+size_x = 102
+size_y = 102
+
+len_x = 5496
+len_y = 3672
+
 image_density_map = np.zeros_like(ccd_2d_bkgdsubtr)
-small_image_density_map = np.zeros([72, 108])
-for y in range(1, 73):
-    for x in range(1, 109):
+small_image_density_map = np.zeros([int(len_y/size_y), int(len_x/size_x)])
+for y in range(1, int(len_y/size_y)+1):
+    for x in range(1, int(len_x/size_x)+1):
         num_stars = 0
         for stars in positions:
-            if (x - 1) * 51 < (stars[0]) <= x * 51 and (y - 1) * 51 < (stars[1]) <= y * 51:
+            if (x - 1) * size_x < (stars[0]) <= x * size_x and (y - 1) * size_y < (stars[1]) <= y * size_y:
                 num_stars += 1
-        image_density_map[(y - 1) * 51:y * 51, (x - 1) * 51:x * 51] = num_stars
+        image_density_map[(y - 1) * size_y:y * size_y, (x - 1) * size_x:x * size_x] = num_stars
         small_image_density_map[(y-1):y, (x-1):x] = num_stars
 
-x_stars = np.linspace(0, 107, 108)
-y_stars = np.linspace(0, 71, 72)
+x_stars = np.linspace(0, int(len_x/size_x), int(len_x/size_x)+1)
+y_stars = np.linspace(0, int(len_y/size_y), int(len_y/size_y)+1)
 xv_stars, yv_stars = np.meshgrid(x_stars, y_stars)
 x_stars_flat = np.hstack(xv_stars)
 y_stars_flat = np.hstack(yv_stars)
@@ -81,8 +88,8 @@ print(x_final, y_final, z_final)
 
 
 # De afbeelding plotten, met colorbar
-fig, axes = plt.subplots(1, 3, figsize=(12, 4))
-fig.tight_layout(w_pad=3)
+fig, axes = plt.subplots(1, 3, figsize=(14, 3))
+fig.tight_layout(w_pad=5, pad=1)
 
 # Labels voor de assen aanmaken
 axes[0].set_ylabel('Y (pixels)')
@@ -109,12 +116,19 @@ axes[1].set_title('Bevolkingsdichtheid kaart dubbelclustert')
 axes[1].set_ylabel('Y (pixels)')
 axes[1].set_xlabel('X (pixels)')
 
-axes[2].plot( y_final, x_final, 'ko', ms=3)
-contour = axes[2].tricontourf(y_final, x_final, z_final, levels=5, cmap='RdBu_r')
-axes[2].set(xlim=(0, 107), ylim=(72, 0))
-
 # colorbar tweede figuur
 cbar2 = fig.colorbar(density, location='right', shrink=0.6)
 cbar2.set_label('# Stars')
+
+#axes[2].plot( y_final, x_final, 'ko', ms=3)
+contour = axes[2].tricontourf(y_final, x_final, z_final, levels=[0, 1.75, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], cmap='RdBu_r', antialiased=False)
+axes[2].set(xlim=(0, int(len_x/size_x)-1), ylim=(int(len_y/size_y)-1, 0))
+axes[2].set_title('Bevolkingsdichtheids contour plot dubbelcuster')
+axes[2].set_ylabel('Y (pixels)')
+axes[2].set_xlabel('X (pixels)')
+
+# colorbar tweede figuur
+cbar3 = fig.colorbar(contour, location='right', shrink=0.6)
+cbar3.set_label('# Stars')
 
 plt.show()
